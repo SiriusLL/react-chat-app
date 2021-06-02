8235d49592cd00876d1b95b450dae861e4397320
SiriusLL <sirius.love.light@gmail.com>
2021-06-02 14:35:14 -0600

    Message && Messages component

--- client/chat-app/src/components/Chat/Chat.js
+++ client/chat-app/src/components/Chat/Chat.js
@@ -6,6 +6,7 @@ import "./Chat.css";
 
 import InfoBar from "../InfoBar/InfoBar";
 import Input from "../Input/Input";
+import Messages from "../Messages/messages";
 
 let socket;
 
@@ -63,6 +64,7 @@ const Chat = ({ location }) => {
     <div className="outerContainer">
       <div className="container">
         <InfoBar room={room} />
+        <Messages messages={messages} />
         <Input
           message={message}
           setMessage={setMessage}
