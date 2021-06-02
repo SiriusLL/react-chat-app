7ee5f66b6fb5b500dead80e4ce4d69a156daf8ab
SiriusLL <sirius.love.light@gmail.com>
2021-06-02 03:00:36 -0600

    socket.on sendMessage

--- server/index.js
+++ server/index.js
@@ -41,6 +41,14 @@ io.on("connect", (socket) => {
     callback();
   });
 
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+    //wait for message to emit
+    io.to(user.room).emit("message", { user: user.name, text: message });
+
+    callback();
+  });
+
   socket.on("disconnect", () => {
     console.log("User disconnect!!");
   });
